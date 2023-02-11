71900591d2724733e990f714d7ace07464c2748b
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2023-02-11 14:05:40 +0100

    Made all registers cleared before jumping to process

--- kernel/process.s
+++ kernel/process.s
@@ -99,4 +99,20 @@ jump_to_program:
   pushf ; RFLAGS
   push SEGMENT_USER_CODE | SEGMENT_RING_3 ; CS
   push rdi ; RIP
+  ; Clear all registers (except RSP)
+  xor rax, rax
+  xor rcx, rcx
+  xor rdx, rdx
+  xor rbx, rbx
+  xor rbp, rbp
+  xor rsi, rsi
+  xor rdi, rdi
+  xor r8, r8
+  xor r9, r9
+  xor r10, r10
+  xor r11, r11
+  xor r12, r12
+  xor r13, r13
+  xor r14, r14
+  xor r15, r15
   iretq
