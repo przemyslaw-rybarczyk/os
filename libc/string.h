877889e8edf7177464efafed7d0b96301acdeea7
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2023-04-07 18:02:38 +0200

    Made memory manipulation functions standard-compliant

--- libc/string.h
+++ libc/string.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void memset(void *dest, int c, size_t n);
-void memcpy(void * restrict dest, const void * restrict src, size_t n);
-void memmove(void *dest, const void *src, size_t n);
+void *memset(void *dest, int c, size_t n);
+void *memcpy(void * restrict dest, const void * restrict src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
--- libc/string.s
+++ libc/string.s
@@ -6,15 +6,18 @@ global memmove
 ; rsi - int c
 ; rdx - size_t n
 memset:
+    mov r8, rdi
     mov rax, rsi
     mov rcx, rdx
     rep stosb
+    mov rax, r8
     ret
 
 ; rdi - void * restrict dest
 ; rsi - const void * restrict src
 ; rdx - size_t n
 memcpy:
+    mov rax, rdi
     mov rcx, rdx
     rep movsb
     ret
@@ -23,6 +26,7 @@ memcpy:
 ; rsi - const void * src
 ; rdx - size_t n
 memmove:
+    mov rax, rdi
     mov rcx, rdx
     cmp rdi, rsi
     jae .forwards
