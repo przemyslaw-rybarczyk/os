63b4e6574c560e98e2cf3dab03231eb8f90b686c
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2023-04-14 11:38:48 +0200

    Added %f specifier to printf()

--- libc/stdio.c
+++ libc/stdio.c
@@ -1,7 +1,9 @@
 #include "types.h"
 #include <stdio.h>
 
+#include <float.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include <syscalls.h>
 
@@ -42,6 +44,12 @@ static void printf_char(struct printf_target target, char c) {
         }
     }
     (*(target.offset))++;
+}
+
+// Print a null-terminated string
+static void printf_string(struct printf_target target, const char *s) {
+    for (const char *p = s; *p != '\0'; p++)
+        printf_char(target, *p);
 }
 
 // Print an unsinged decimal number
@@ -113,6 +121,166 @@ static void printf_hex(struct printf_target target, uintmax_t n, bool uppercase)
     }
 }
 
+// Used to cast a long double into its components
+union long_double_cast {
+    long double ld;
+    struct {
+        u64 mantissa;
+        u16 sign_exponent;
+    } __attribute__((packed));
+};
+
+// Verify that long double is 80-bit extended precision format
+// The code for printing floating point numbers relies on this assumption.
+#if !(FLT_RADIX == 2 && LDBL_MANT_DIG == 64 && LDBL_MIN_EXP == -16381 && LDBL_MAX_EXP == 16384)
+#error "long double is not 80-bit extended precision format"
+#endif
+
+// 10^19 - the largest power of 10 that can fit in a u64
+#define POW_10_19 10000000000000000000ull
+
+// Print a floating-point number
+// `uppercase` determines whether nan and inf are printed in lowercase or uppercase.
+// The conversion algorithm used is very basic and does not properly round the result, but only truncates it.
+static void printf_float(struct printf_target target, long double f, bool uppercase) {
+    // Extract mantissa and exponent fields
+    union long_double_cast f_cast;
+    f_cast.ld = f;
+    u64 mantissa = f_cast.mantissa;
+    u16 exponent_field = f_cast.sign_exponent & 0x7FFF;
+    // If the sign bit is set, print a minus sign
+    if (f_cast.sign_exponent & 0x8000)
+        printf_char(target, '-');
+    // Handle NaN and infinity
+    if (exponent_field == 0x7FFF) {
+        // When checking the mantissa, ignore the highest bit, since it should be 1
+        if ((mantissa & 0x7FFFFFFFFFFFFFFF) == 0)
+            printf_string(target, uppercase ? "INF" : "inf");
+        else
+            printf_string(target, uppercase ? "NAN" : "nan");
+        return;
+    }
+    // Subtract the bias from the exponent
+    i32 exponent = exponent_field - 16383;
+    // If the number is denormal, adjust the exponent to account for it
+    // There is no need to change anything else, since the integral part of the mantissa is already explicitly stored in this format.
+    if (exponent_field == 0) {
+        exponent += 1;
+    }
+    // Buffer for storing the expanded form of the integral part or the fractional part of the floating-point number
+    // Must be long enough to hold -(maximum exponent) + (length of mantissa) - 1 bits = (16383 + 64 - 1) / 8 qwords = 256.96875 qwords
+    u64 fp_digits[257];
+    int fp_digits_num;
+    // Buffer for storing the groups of digits created when converting the integral part to decimal
+    // Each element represents 19 decimal digits.
+    // Must be long enough to hold log (2^16383) base (10^19) elements ≈ 259.56707 elements
+    u64 dec_digit_groups[260];
+    int dec_digit_groups_num;
+    // Buffer for storing the digits created when converting a digit group to its individual digits
+    char dec_digits[19];
+    // Convert the integral part to a little-endian big integer
+    // This involves shifting the mantissa left by the exponent
+    fp_digits_num = exponent / 64 + 1;
+    if (exponent < 0) {
+        // The mantissa is entirely within the fractional part of the number, so the integral part is zero
+        fp_digits_num = 0;
+    } else if (exponent < 64) {
+        // The mantissa is split between the integral and fractional part of the number
+        fp_digits[0] = mantissa >> (63 - exponent);
+    } else if (exponent % 64 == 63) {
+        // The mantissa lies within a single u64
+        fp_digits[exponent / 64] = mantissa;
+        memset(fp_digits, 0, sizeof(u64) * (exponent / 64));
+    } else {
+        // The mantissa is split between two u64s
+        fp_digits[exponent / 64] = mantissa >> (63 - exponent % 64);
+        fp_digits[exponent / 64 - 1] = mantissa << (exponent % 64 + 1);
+        memset(fp_digits, 0, sizeof(u64) * (exponent / 64 - 1));
+    }
+    // Convert the integral part to decimal
+    // We divide by 10^19 instead of 10 to produce 19 digits at a time.
+    dec_digit_groups_num = 0;
+    while (fp_digits_num > 0) {
+        // Divide the integral part by 10^19 using long division
+        u64 remainder = 0;
+        for (int i = fp_digits_num - 1; i >= 0; i--) {
+            // Set fp_digits[i] = remainder:fp_digits[i] / POW_10_19 and remainder = remainder:fp_digits[i] % POW_10_19 (":" indicates concatenation)
+            // This is done in inline assembly since there isn't a good way to divide a 128-bit integer by a 64-bit integer to get a 64-bit result in C.
+            asm ("div %[d]"
+                : "=a"(fp_digits[i]), "=d"(remainder)
+                : [d] "r"(POW_10_19), "d"(remainder), "a"(fp_digits[i])
+            );
+        }
+        // The final remainder is the next digit
+        dec_digit_groups[dec_digit_groups_num++] = remainder;
+        // If the highest u64 became zero, shrink the bignum's length
+        if (fp_digits[fp_digits_num - 1] == 0)
+            fp_digits_num -= 1;
+    }
+    // Print the integral part
+    if (dec_digit_groups_num == 0) {
+        // If the integral part is zero, print a zero
+        printf_char(target, '0');
+    } else {
+        // Print the first digit group while skipping initial zeroes
+        size_t n = dec_digit_groups[dec_digit_groups_num - 1];
+        int i = 0;
+        for (; n > 0; n /= 10)
+            dec_digits[i++] = (n % 10) + '0';
+        for (int j = 0; j < i; j++)
+            printf_char(target, dec_digits[i - j - 1]);
+        // Print the remaining digit groups
+        for (int i = dec_digit_groups_num - 2; i >= 0; i--) {
+            u64 n = dec_digit_groups[i];
+            for (int j = 0; j < 19; j++) {
+                dec_digits[j] = (n % 10) + '0';
+                n /= 10;
+            }
+            for (int j = 0; j < 19; j++)
+                printf_char(target, dec_digits[18 - j]);
+        }
+    }
+    // Print the decimal point
+    printf_char(target, '.');
+    // Convert the fractional part to a big-endian big integer
+    // This involves shifting the mantissa left by the negated exponent
+    fp_digits_num = (- exponent + 62) / 64 + 1;
+    if (exponent >= 63) {
+        // The mantissa is entirely within the integral part of the number, so the fractional part is zero
+        fp_digits_num = 0;
+    } else if (exponent >= -1) {
+        // The mantissa is split between the integral and fractional part of the number
+        fp_digits[0] = mantissa << (exponent + 1);
+    } else if (exponent % 64 == -1) {
+        // The mantissa lies within a single u64
+        fp_digits[(- exponent - 1) / 64] = mantissa;
+        memset(fp_digits, 0, sizeof(u64) * ((- exponent - 1) / 64));
+    } else {
+        // The mantissa is split between two u64s
+        fp_digits[(- exponent - 1) / 64] = mantissa >> ((- exponent - 1) % 64);
+        fp_digits[(- exponent - 1) / 64 + 1] = mantissa << (64 - (- exponent - 1) % 64);
+        memset(fp_digits, 0, sizeof(u64) * ((- exponent - 1) / 64));
+    }
+    // Multiply the fractional part by 10^19 using long multiplication to get the highest 19 digits
+    u64 remainder = 0;
+    for (int i = fp_digits_num - 1; i >= 0; i--) {
+        // Set remainder:fp_digits[i] = fp_digits[i] * POW_10_19 + remainder (":" indicates concatenation)
+        // As with the division, there is no good way to this is C, so inline assembly is used.
+        asm ("mul %[m]; add rax, %[r]; adc rdx, 0"
+            : "=d"(remainder), "=a"(fp_digits[i])
+            : [m] "r"(POW_10_19), [r] "r"(remainder), "a"(fp_digits[i])
+        );
+    }
+    // Convert the remainder to digits and print the first 6
+    u64 n = remainder;
+    for (int j = 0; j < 19; j++) {
+        dec_digits[j] = (n % 10) + '0';
+        n /= 10;
+    }
+    for (int j = 0; j < 6; j++)
+        printf_char(target, dec_digits[18 - j]);
+}
+
 // Print to a printf target
 // This function implements the main logic of all printf() family functions.
 static void printf_common(struct printf_target target, const char *restrict fmt, va_list args) {
@@ -140,8 +308,7 @@ static void printf_common(struct printf_target target, const char *restrict fmt,
         }
         case 's': {
             const char *s = va_arg(args, const char *);
-            for (const char *p = s; *p != '\0'; p++)
-                printf_char(target, *p);
+            printf_string(target, s);
             break;
         }
         case 'd':
@@ -168,6 +335,16 @@ static void printf_common(struct printf_target target, const char *restrict fmt,
         case 'u': {
             unsigned int n = va_arg(args, unsigned int);
             printf_dec(target, n);
+            break;
+        }
+        case 'f': {
+            double f = va_arg(args, double);
+            printf_float(target, f, false);
+            break;
+        }
+        case 'F': {
+            double f = va_arg(args, double);
+            printf_float(target, f, true);
             break;
         }
         // Incorrect specifiers have undefined behavior, so we choose to ignore them
--- program1/main.c
+++ program1/main.c
@@ -1,11 +1,16 @@
+#include <float.h>
 #include <stdio.h>
 
 char buf[20];
 
 void main(void) {
-    int x = printf("AAA %% %d %d 0x%x 0x%X 0%o %s%c %s%c\n", 1234, -1234, 0x1a2b, 0x1A2B, 01234, "Hello", ',', "World", '!');
-    int y = snprintf(buf, 20, "AAA %% %d %d 0x%x 0x%X 0%o %s%c %s%c\n", 1234, -1234, 0x1a2b, 0x1A2B, 01234, "Hello", ',', "World", '!');
-    puts(buf);
-    putchar('\n');
-    printf("%d %d\n", x, y);
+    printf("%f\n", 12.3456789012);
+    printf("%f\n", -0.00001234567890123456789);
+    printf("%f\n", 0.0);
+    printf("%f\n", -0.0);
+    printf("%f\n", DBL_MAX);
+    printf("%f\n", 1.0 / 0.0);
+    printf("%F\n", -1.0 / 0.0);
+    printf("%F\n", 0.0 / 0.0);
+    printf("%f\n", -(0.0 / 0.0));
 }
