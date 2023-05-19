26170328c322770be558943a153fc219154af210
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2023-05-19 21:53:02 +0200

    Removed nonfuctional error message printing code from _start

--- libc/crt0.s
+++ libc/crt0.s
@@ -4,10 +4,6 @@ extern main
 extern _string_init
 extern _alloc_init
 
-section .rodata
-
-error_msg: db `Error: failed to initialize process\n\0`
-
 section .text
 
 STACK_START equ 0x00007FFFFFF00000
@@ -16,10 +12,7 @@ STACK_LENGTH equ 0x100000
 MAP_PAGES_WRITE equ 1
 
 SYSCALL_MAP_PAGES equ 0
-SYSCALL_PRINT_CHAR equ 1
-SYSCALL_PROCESS_EXIT equ 2
-SYSCALL_MESSAGE_GET_LENGTH equ 4
-SYSCALL_MESSAGE_READ equ 5
+SYSCALL_PROCESS_EXIT equ 1
 
 _start:
   ; Map the stack
@@ -45,13 +38,4 @@ _start:
   mov rax, SYSCALL_PROCESS_EXIT
   syscall
 .fail:
-  ; Print the error message and exit
-  mov rdx, error_msg
-.print_loop:
-  mov rdi, [rdx]
-  test rdi, rdi
-  jz .exit
-  mov rax, SYSCALL_PRINT_CHAR
-  syscall
-  add rdx, 1
-  jmp .print_loop
+  jmp .exit
